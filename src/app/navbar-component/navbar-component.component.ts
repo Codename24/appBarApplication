14075ea7b5995e7990bdd0e3fb4df49c61d4e76e
Markiian Yuskiv <codename24j@gmail.com>
2017-01-23 12:16:14 -0700

    Updated search form

--- src/app/navbar-component/navbar-component.component.ts
+++ src/app/navbar-component/navbar-component.component.ts
@@ -13,14 +13,13 @@ import {
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { SearchComponent } from '../search/search.component';
 import {DropdownComponent} from '../dropdown/dropdown.component';
-import {DropdownValue} from '../../app/dropdownvalue'
+import {DropdownValue} from '../../app/dropdownvalue';
+
 
 @Component({
   selector: 'app-navbar-component',
   templateUrl: './navbar-component.component.html',
   styleUrls: ['./navbar-component.component.css', '../../app/font.css'],
-  
-  
   animations: [
     trigger('searchComponentTrigger', [
       state('closed', style({
@@ -31,8 +30,8 @@ import {DropdownValue} from '../../app/dropdownvalue'
         display: 'block'
       })),
       ,
-      transition('closed => opened', animate('400ms ease-in-out')),
-      transition('opened => closed', animate('400ms ease-in-out'))
+      transition('closed => opened', animate('20ms ease-in-out')),
+      transition('opened => closed', animate('20ms ease-in-out'))
     ]),
     trigger('buttonTrigger', [
       state('closed', style({
@@ -41,11 +40,12 @@ import {DropdownValue} from '../../app/dropdownvalue'
         width: '50px',
         background:'#194062',
         outline:'none',
-        position:'relative'           
+        position:'absolute',
+        right:'400px'
       }))
       ,
-      transition('closed => opened', animate('400ms ease-in-out')),
-      transition('opened => closed', animate('400ms ease-in-out'))
+      transition('closed => opened', animate('20ms ease-in-out')),
+      transition('opened => closed', animate('20ms ease-in-out'))
     ]),
     
     trigger('sidebarComponentTrigger', [
@@ -69,8 +69,8 @@ import {DropdownValue} from '../../app/dropdownvalue'
         display: 'block'
       })),
       ,
-      transition('closed => opened', animate('200ms ease-in-out')),
-      transition('opened => closed', animate('200ms ease-in-out'))
+      transition('closed => opened', animate('50ms ease-in-out')),
+      transition('opened => closed', animate('50ms ease-in-out'))
     ])
   ]
 
@@ -79,13 +79,12 @@ export class NavbarComponentComponent implements OnInit {
 
   constructor() { }
   state: string = 'closed';
-  firsDropDownState:string = 'closed';
-  secondDropDownState:string = 'closed'
-  sidebarState: string = 'closed'
-  buttonValue:string ='< Go to a matter...';
-  documentsDropDownValues:DropdownValue[]; 
-
- 
+  firsDropDownState: string = 'closed';
+  secondDropDownState: string = 'closed';
+  sidebarState: string = 'closed';
+  buttonValue: string = 'Go to a matter...';
+  buttonIcon: string = 'fa-angle-left';
+  documentsDropDownValues:DropdownValue[];
   ngOnInit() {
    this.documentsDropDownValues = [
   new DropdownValue("#","Recent"),
@@ -93,15 +92,18 @@ export class NavbarComponentComponent implements OnInit {
   }
   toggleMenu() {
     this.state = this.state == 'closed' ? 'opened' : "closed";
-    this.buttonValue = this.state=='closed'?' Go to a matter...':'>'
+    this.buttonValue = this.state=='closed'?'Go to a matter...': '';
+    this.buttonIcon = this.state === 'closed' ? 'fa-angle-left' : 'fa-angle-down';
   }
 
   openDocumentsDrodown() {
   this.firsDropDownState = this.firsDropDownState == 'closed' ? 'opened' : "closed";
+  this.secondDropDownState = 'closed';
   }
 
   openMattersDrodown() {
     this.secondDropDownState= this.secondDropDownState == 'closed' ? 'opened' : "closed";
+     this.firsDropDownState = 'closed'; 
   }
 
   toggleNavBar() {
