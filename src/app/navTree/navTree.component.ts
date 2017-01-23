fa52529a7d6b6427eb389ec104058bc9de39438f
lowellnelson <lowell@netdocuments.com>
2017-01-23 13:47:55 -0700

    Add navTree component family

---
+++ src/app/navTree/navTree.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navTree',
+  templateUrl: './navTree.component.html',
+  styleUrls: ['./navTree.component.css']
+})
+export class NavTreeComponent implements OnInit {
+
+  constructor() { 
+  }
+
+  ngOnInit() {
+  }
+
+}
