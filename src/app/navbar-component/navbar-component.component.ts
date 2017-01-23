6710e32603461bb271a04fc9e5110f5fc2490724
Markiian Yuskiv <markiianyuskiv@Markiians-MacBook-Pro.local>
2017-01-23 00:30:41 -0700

    Added changes regarding dropdonws

--- src/app/navbar-component/navbar-component.component.ts
+++ src/app/navbar-component/navbar-component.component.ts
@@ -6,14 +6,20 @@ import {
   style,
   transition,
   animate,
-  Input
+  Input,
+  Optional,
+  Inject
 } from '@angular/core';
-import { SearchComponent } from '../search/search.component'
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { SearchComponent } from '../search/search.component';
+import {DropdownComponent} from '../dropdown/dropdown.component';
+import {DropdownValue} from '../../app/dropdownvalue'
 
 @Component({
   selector: 'app-navbar-component',
   templateUrl: './navbar-component.component.html',
   styleUrls: ['./navbar-component.component.css', '../../app/font.css'],
+  // providers:[DropdownValue],
   animations: [
     trigger('searchComponentTrigger', [
       state('closed', style({
@@ -29,20 +35,18 @@ import { SearchComponent } from '../search/search.component'
     ]),
     trigger('buttonTrigger', [
       state('closed', style({
-       
       })),
       state('opened', style({
         width: '50px',
         background:'#194062',
         outline:'none',
-        position:'relative' 
-        
-        
+        position:'relative'           
       }))
       ,
       transition('closed => opened', animate('400ms ease-in-out')),
       transition('opened => closed', animate('400ms ease-in-out'))
-    ])
+    ]),
+    
 
 
   ]
@@ -53,11 +57,19 @@ export class NavbarComponentComponent implements OnInit {
   constructor() { }
   state: string = 'closed'
   buttonValue:string ='< Go to a matter...';
+  documentsDropDownValues = [
+  new DropdownValue("#","Recent"),
+  new DropdownValue("#","Favourites")];
+
+ 
   ngOnInit() {
   }
   toggleMenu() {
     this.state = this.state == 'closed' ? 'opened' : "closed";
     this.buttonValue = this.state=='closed'?' Go to a matter...':'>'
+  }
+  openDrodown(){
+
   }
   openNav() {
 
